ac1747b98583b75d098c1431b7dcf44dfad579aa
Elisabeth Bonnevier <elisabeth@stenholm.one>
2024-09-10 16:02:07 +0200

    Add optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
 import Lib
+import Options.Applicative
 
 main :: IO ()
 main = someFunc
+
+opts :: ParserInfo ()
+opts =
+  info
+    _
+    (fullDesc <> header "The Fix - correct console commands")
