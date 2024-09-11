f1bc012cbad3a2ab5fa69320dfd464ca043a9e7d
Elisabeth Bonnevier <elisabeth@stenholm.one>
2024-09-11 19:37:41 +0200

    Create JSON data given a command

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,38 @@
 module Main (main) where
 
 import Lib
+import Data.Aeson
 import Options.Applicative
 
 main :: IO ()
 main = do
-  command <- customExecParser (prefs (showHelpOnError <> showHelpOnEmpty)) opts
-  putStrLn command
+  (command, apikey) <- customExecParser (prefs (showHelpOnError <> showHelpOnEmpty)) opts
+  print $ encode $ requestBody command
 
+-- | Parse a command
 parseCommand :: Parser Command
 parseCommand =
   strArgument
     (metavar "COMMAND" 
     <> help "The command to be corrected")
 
-opts :: ParserInfo Command
+-- | Parse an OpenAI api key (only for development)
+parseAPIKey :: Parser String
+parseAPIKey =
+  strArgument
+    (metavar "API_KEY"
+    <> help "OpenAI api key (temporary)")
+
+-- | Parse a command and an OpenAI api key (only for development)
+parseCommandAndAPIKey :: Parser (Command, String)
+parseCommandAndAPIKey =
+  liftA2 (,) parseCommand parseAPIKey
+
+-- | Description of the program, with options
+opts :: ParserInfo (Command , String)
 opts =
   info
-    (parseCommand <**> helper)
+    (parseCommandAndAPIKey <**> helper) -- ^Change to only parse command (development)
     (fullDesc 
     <> progDesc "The Fix is a command line program which corrects the previous command." 
     <> header "The Fix - correct console commands")
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,74 @@
-module Lib (Command) where
+{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
 
-type Command = String
+module Lib (Command, requestBody) where
+
+import GHC.Generics
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Data.Aeson
+import Network.HTTP.Req
+
+-- | A console command
+type Command = Text
+
+-- | Role used in API request
+data Role = 
+  System -- ^To set the behavior of the model
+  | User -- ^Message for model to respond to
+  deriving (Eq, Ord, Show)
+
+instance ToJSON Role where
+  toJSON System = toJSON ("system" :: Text)
+  toJSON User = toJSON ("user" :: Text)
+
+-- | Message given to the model
+data Message =
+  Message {
+    role :: Role,
+    content :: Text
+  }
+  deriving (Eq, Ord, Show, Generic)
+
+instance ToJSON Message where
+  toEncoding = genericToEncoding defaultOptions
+
+-- | GPT model
+data Model =
+  FourO -- ^4o model
+  | FourOMini -- ^4o-mini model
+  deriving (Eq, Ord, Show)
+
+instance ToJSON Model where
+  toJSON FourO = toJSON ("gpt-4o" :: Text)
+  toJSON FourOMini = toJSON ("gpt-4o-mini" :: Text)
+
+-- | Body of API request
+data Body =
+  Body {
+    model :: Model,
+    messages :: [Message]
+  }
+  deriving (Eq, Ord, Show, Generic)
+
+instance ToJSON Body where
+  toEncoding = genericToEncoding defaultOptions
+
+-- | Messages to model, given a command to be corrected
+requestMessages :: Command -> [Message]
+requestMessages command =
+  [
+    Message {
+      role = User,
+      content = 
+        Text.concat
+          [
+            "Correct the following terminal command: `",
+            command,
+            "`."
+          ]
+      }
+  ]
+
+requestBody :: Command -> Body
+requestBody =
+  Body FourO . requestMessages
