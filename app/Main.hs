fa9f0068ae1f02c101183c0450e03240d1f40f60
Elisabeth Stenholm <elisabeth@stenholm.one>
2024-09-15 14:40:41 +0200

    Run suggestion with graceful exception handling

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,38 @@
-module Main (main) where
+module Main
+  ( main
+  ) where
 
-import Request
-import Options.Applicative
-import Control.Monad.IO.Class
-import Data.ByteString (ByteString)
+import           Control.Exception
+import           Control.Monad
 import qualified Data.ByteString.Char8 as ByteString
-import qualified Data.Text as Text
-import Data.Aeson
-import Network.HTTP.Req hiding (header)
-import System.Console.ANSI
-import System.IO
-import Distribution.Simple.Utils hiding (info)
+import           Data.Maybe
+import           Data.Text             (Text)
+import           Network.HTTP.Req      hiding (header)
+import           Options.Applicative   hiding (command)
+import           Request
+import           System.Console.ANSI
+import           System.Environment
+import           System.Exit
+import           System.IO
+import           System.Process
 
 main :: IO ()
 main = do
-  (command, prevOutput, apikey) <- customExecParser (prefs (showHelpOnError <> showHelpOnEmpty)) opts
-  response <- runReq defaultHttpConfig $ request (requestBody command (Text.pack prevOutput)) (ByteString.pack apikey)
-  case safeHead $ suggestions (responseBody response) of
-    Nothing -> putStrLn "No suggestions."
-    Just suggestion -> simpleTui suggestion
+  args <- customExecParser (prefs (showHelpOnError <> showHelpOnEmpty)) opts
+  let varName = apiKeyVarName args
+  maybeApiKey <- lookupEnv $ varName
+  case maybeApiKey of
+    Nothing -> putStrLn $ "Error: No environment variable " ++ varName ++ "."
+    Just apiKey -> do
+      response <-
+        runReq defaultHttpConfig $
+        request (requestBody (command args)) (ByteString.pack apiKey)
+      case listToMaybe $ suggestions (responseBody response) of
+        Nothing -> putStrLn "No suggestions."
+        Just suggestion -> do
+          catch (simpleTui suggestion) handleUserInterrupt
+          _ <- try (callCommand suggestion) :: IO (Either IOError ())
+          return ()
 
 -- | Print suggested command and keys for accepting or rejecting the suggestion
 --   Only for single suggestion
@@ -29,53 +43,52 @@ simpleTui suggestion = do
   if stdoutSupportsANSI
     then do
       putStr $ suggestion ++ " ["
-      setSGR [SetColor Foreground Dull Green]
+      setSGR [SetColor Foreground Vivid Green]
       putStr "Enter"
       setSGR [Reset]
       putStr "/"
-      setSGR [SetColor Foreground Dull Red]
+      setSGR [SetColor Foreground Vivid Red]
       putStr "Ctrl+C"
       setSGR [Reset]
       putStr "]"
-    else
-      putStr $ suggestion ++ " [Ctrl+C/Enter]"
+    else putStr $ suggestion ++ " [Ctrl+C/Enter]"
   hFlush stdout
   _ <- getLine
-  return ()
+  putStr "\n"
+  hSetEcho stdin True
 
--- | Parse a command
-parseCommand :: Parser Command
-parseCommand =
-  strArgument
-    (metavar "COMMAND" 
-    <> help "The command to be corrected")
+handleUserInterrupt :: AsyncException -> IO ()
+handleUserInterrupt UserInterrupt = do
+  stdoutSupportsANSI <- hNowSupportsANSI stdout
+  when stdoutSupportsANSI (setSGR [SetColor Foreground Vivid Red])
+  putStrLn "\nAborted."
+  exitSuccess
+handleUserInterrupt e = throwIO e
 
--- | Parse output from previous command
-parseOutput :: Parser String
-parseOutput =
-  strArgument
-    (metavar "PREV_OUTPUT"
-    <> help "Output from previous command"
-    <> showDefault
-    <> value "")
+-- | All arguments to the program
+data Arguments =
+  Arguments
+    { command       :: Text -- ^The command to be corrected
+    , apiKeyVarName :: String -- ^Name of environment variable containing OpenAI api key
+    }
 
--- | Parse an OpenAI api key (only for development)
-parseAPIKey :: Parser String
-parseAPIKey =
-  strArgument
-    (metavar "API_KEY"
-    <> help "OpenAI api key (temporary)")
-
--- | Parse a command and an OpenAI api key (only for development)
-parseAll :: Parser (Command, String, String)
-parseAll =
-  liftA3 (,,) parseCommand parseOutput parseAPIKey
+-- | Parse arguments to the program
+parseArguments :: Parser Arguments
+parseArguments =
+  Arguments <$>
+  strArgument (metavar "COMMAND" <> help "The command to be corrected") <*>
+  strOption
+    (long "var-name" <>
+     short 'n' <>
+     help "Name of environment variable containing OpenAI api key" <>
+     showDefault <> value "OPENAI_API_KEY" <> metavar "NAME")
 
 -- | Description of the program, with options
-opts :: ParserInfo (Command, String, String)
+opts :: ParserInfo Arguments
 opts =
   info
-    (parseAll <**> helper) -- ^Change to only parse command (development)
-    (fullDesc 
-    <> progDesc "The Fix is a command line program which corrects the previous command." 
-    <> header "The Fix - correct console commands")
+    (parseArguments <**> helper)
+    (fullDesc <>
+     progDesc
+       "The Fix is a command line program which corrects the previous command." <>
+     header "The Fix - correct console commands")
--- src/Request.hs
+++ src/Request.hs
@@ -1,96 +1,84 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- | This module constructs the API request and interprets the response
-
-module Request (
-  Command,
-  requestBody,
-  request,
-  suggestions
+module Request
+  ( requestBody
+  , request
+  , suggestions
   ) where
 
-import GHC.Generics
-import Data.Text (Text)
-import qualified Data.Text as Text
-import Data.ByteString (ByteString)
-import Data.Aeson
-import Data.Aeson.Types
-import Data.Vector
-import Network.HTTP.Req
-import Lens.Micro.Platform
-import Lens.Micro.Aeson
-
--- | A console command
-type Command = Text
+import           Data.Aeson
+import           Data.ByteString     (ByteString)
+import           Data.Text           (Text)
+import qualified Data.Text           as Text
+import           GHC.Generics
+import           Lens.Micro.Aeson
+import           Lens.Micro.Platform
+import           Network.HTTP.Req
 
 -- | Role used in API request
-data Role = 
-  System -- ^To set the behavior of the model
+data Role
+  = System -- ^To set the behavior of the model
   | User -- ^Message for the model to respond to
   deriving (Eq, Ord, Show)
 
 instance ToJSON Role where
   toJSON System = toJSON ("system" :: Text)
-  toJSON User = toJSON ("user" :: Text)
+  toJSON User   = toJSON ("user" :: Text)
 
 -- | Message given to the model
 data Message =
-  Message {
-    role :: Role,
-    content :: Text
-  }
+  Message
+    { role    :: Role
+    , content :: Text
+    }
   deriving (Eq, Ord, Show, Generic)
 
 instance ToJSON Message where
   toEncoding = genericToEncoding defaultOptions
 
 -- | GPT model
-data Model =
-  FourO -- ^4o model
+data Model
+  = FourO -- ^4o model
   | FourOMini -- ^4o-mini model
   deriving (Eq, Ord, Show)
 
 instance ToJSON Model where
-  toJSON FourO = toJSON ("gpt-4o" :: Text)
+  toJSON FourO     = toJSON ("gpt-4o" :: Text)
   toJSON FourOMini = toJSON ("gpt-4o-mini" :: Text)
 
 -- | Body of API request
 data Body =
-  Body {
-    model :: Model,
-    messages :: [Message]
-  }
+  Body
+    { model    :: Model
+    , messages :: [Message]
+    }
   deriving (Eq, Ord, Show, Generic)
 
 instance ToJSON Body where
   toEncoding = genericToEncoding defaultOptions
 
 -- | Messages to model, containing the command to be corrected
-requestMessages :: Command -> Text -> [Message]
-requestMessages command prevOutput =
-  [
-    Message {
-      role = User,
-      content = 
-        Text.concat
-          [
-            "Correct the following terminal command: `",
-            command,
-            "`. The output of the command was:\n\n```",
-            prevOutput,
-            "```\n\n",
-            "Give me nothing but the command as text, not as a code block."
-          ]
+requestMessages :: Text -> [Message]
+requestMessages command =
+  [ Message
+      { role = User
+      , content =
+          Text.concat
+            [ "Correct the following terminal command: `"
+            , command
+            , "`. Give me nothing but the command as text, not as a code block."
+            ]
       }
   ]
 
 -- | Body of request to model
-requestBody :: Command -> Text -> Body
-requestBody = (Body FourO .) . requestMessages
+requestBody :: Text -> Body
+requestBody = Body FourO . requestMessages
 
--- | API request
-request :: (MonadHttp m, ToJSON a, FromJSON b) 
-        => a -> ByteString -> m (JsonResponse b)
+-- | API request to model
+request :: Body -> ByteString -> Req (JsonResponse Value)
 request body apikey =
   req
     POST
@@ -102,4 +90,6 @@ request body apikey =
 -- | Retrieve command suggestions from response
 suggestions :: Value -> [String]
 suggestions =
-  toListOf (key "choices" . _Array . each . key "message" . key "content" . _String . unpacked)
+  toListOf
+    (key "choices" .
+     _Array . each . key "message" . key "content" . _String . unpacked)
